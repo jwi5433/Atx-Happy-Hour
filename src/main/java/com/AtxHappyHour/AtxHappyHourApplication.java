679677cf4d6e4d8eda24e813b9a063d47cd2d77e
john <jmwilliams0604@gmail.com>
2023-11-26 12:18:38 -0600

    initial commit

---
+++ src/main/java/com/AtxHappyHour/AtxHappyHourApplication.java
@@ -0,0 +1,13 @@
+package com.AtxHappyHour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtxHappyHourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtxHappyHourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AtxHappyHour/AtxHappyHourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AtxHappyHour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtxHappyHourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

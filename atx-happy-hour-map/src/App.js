0d205a3f75376ccc74986da33458af87b510df78
John <jmwilliams0604@gmail.com>
2023-12-01 14:41:41 -0600

    Linked url to backend

--- atx-happy-hour-map/src/App.js
+++ atx-happy-hour-map/src/App.js
@@ -28,7 +28,7 @@ const handleSearchChange = async (e) => {
     setSearchTerm(value);
 
     if (value.length > 2) {
-        const response = await fetch(`/api/restaurants/searchByName?name=${encodeURIComponent(value)}`);
+        const response = await fetch(`https://atxhappyhour.onrender.com/api/restaurants/searchByName?name=${encodeURIComponent(value)}`);
         const data = await response.json();
         setSuggestions(data);
     } else {
@@ -47,7 +47,7 @@ const handleSearchSubmit = (e) => {
 
 const fetchRestaurants = async () => {
     try {
-        const response = await fetch('/api/restaurants');
+        const response = await fetch('https://atxhappyhour.onrender.com/api/restaurants');
         if (!response.ok) {
             throw new Error(`Error: ${response.status}`);
         }
